5fecfa01043212fa7636c96a443146e80b702eb6
Naive Roboticist <naiveroboticist@gmail.com>
2014-06-24 19:09:29 -0400

    Cleaned up error handling.
    Made the receipt of messages more robust. If there is an error
    while waiting for a message (like the socket gets closed), we
    now deal with the issue by logging it to the syslog and letting
    the ranch working just fall off the loop. Clean. Elegant.

--- apps/telep/src/robot_command_protocol.erl
+++ apps/telep/src/robot_command_protocol.erl
@@ -12,13 +12,14 @@ start_link(Ref, Socket, Transport, Opts) ->
 init(Ref, Socket, Transport, _Opts = []) ->
 	ok = ranch:accept_ack(Ref),
   { ok, Q } = beanstalk:connect(),
+  { ok, Log } = syslog:open("robot_command_protocol", [ cons, perror, pid ], local3),
   { using, "registration" } = beanstalk:use(Q, registration),
   % { watching, _ } = beanstalk:watch(Q, commands),
-	loop(init, Socket, Transport, Q, nil),
+	loop(init, Socket, Transport, Q, Log, nil),
 	ok = Transport:close(Socket),
   ok = beanstalk:close(Q).
 
-loop(init, Socket, Transport, Q, _) ->
+loop(init, Socket, Transport, Q, Log, _) ->
   send_message(Socket, Transport, "And You Are?"),
   case receive_message(Socket, Transport) of
     { ok, Data } ->
@@ -26,37 +27,49 @@ loop(init, Socket, Transport, Q, _) ->
         [ "robot", RobotName ] ->
           % Valid challenge response. Put a message in the queue
           { inserted, _ } = beanstalk:put(Q, binary:list_to_bin(io_lib:format("register|~s", [ RobotName ]))),
-          loop(command_wait, Socket, Transport, Q, RobotName);
+          loop(command_wait, Socket, Transport, Q, Log, RobotName);
         _ ->
-          io:format("Invalid challenge response: ~s~n", [ Data ])
+          syslog:log(Log, notice, "Invalid challenge response: ~s", [ Data ])
       end;
     Error ->
-      io:format("Error receiving init message: ~p~n", [ Error ])
+      syslog:log(Log, notice, "Error receiving init message: ~p", [ Error ])
   end;
 
-loop(command_wait, Socket, Transport, Q, TubeName) ->
+loop(command_wait, Socket, Transport, Q, Log, TubeName) ->
   { watching, _ } = beanstalk:watch(Q, io_lib:format("~s_commands", [ TubeName ])),
   case beanstalk:reserve(Q) of
     { reserved, _JobId, BMessage } ->
       send_message(Socket, Transport, binary:bin_to_list(BMessage)),
-      loop(command_wait, Socket, Transport, Q, TubeName);
+      loop(command_wait, Socket, Transport, Q, Log, TubeName);
     Error ->
-      io:format("Error reserving a job: ~p~n",  [ Error ])
+      syslog:log(Log, err, "Error reserving a job: ~p",  [ Error ])
   end.
 
 %% Private methods
 
 receive_message(Socket, Transport) ->
-  { ok, Length } = receive_message_length(Socket, Transport),
-  receive_message_content(Socket, Transport, Length).
+  case receive_message_length(Socket, Transport) of
+    { ok, Length } ->
+      receive_message_content(Socket, Transport, Length);
+    Error ->
+      Error
+  end.
 
 receive_message_length(Socket, Transport) ->
-  { ok, BLength } = Transport:recv(Socket, 1, 10000),
-  { ok, binary:decode_unsigned(BLength) }.
+  case Transport:recv(Socket, 1, 10000) of
+    { ok, BLength } ->
+      { ok, binary:decode_unsigned(BLength) };
+    Error ->
+      Error
+  end.
 
 receive_message_content(Socket, Transport, Length) ->
-  { ok, BString } = Transport:recv(Socket, Length, 10000),
-  { ok, binary:bin_to_list(BString) }.
+  case Transport:recv(Socket, Length, 10000) of
+    { ok, BString } ->
+      { ok, binary:bin_to_list(BString) };
+    Error ->
+      Error
+  end.
 
 send_messages(_, _, []) ->
   io:format("No commands to send~n");
