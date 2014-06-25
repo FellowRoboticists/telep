ba600b78c3fe08ca24ad440e2f67317623eb3e84
Dave Sieh <dj0hnve@gmail.com>
2014-06-25 17:41:34 -0600

    Updated the client for arguments.

--- js/robot_client.js
+++ js/robot_client.js
@@ -1,18 +1,22 @@
 var net = require('net');
 var sprintf = require('sprintf').sprintf;
 
+var args = process.argv.slice(2);
+
 var client = new net.Socket();
 var first = true;
-var name = "robot|Sammy";
+// var name = "robot|Sammy";
+var name = args[0] || "Sammy";
+var registrationStr = sprintf("robot|%s", name);
 
-client.connect(5555, 'localhost', function() {
+client.connect(5555, 'daneel', function() {
     console.log('Connected');
 });
 
 client.on('data', function(data) {
     console.log('Data: ' + data);
     if (first) {
-      client.write(sprintf("%c%s", name.length, name));
+      client.write(sprintf("%c%s", registrationStr.length, registrationStr));
       first = false;
     }
     // client.destroy();
