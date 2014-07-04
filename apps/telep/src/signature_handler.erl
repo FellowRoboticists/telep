f62b71af7eb70f7ce954c2a32432f74bcd0485ca
Naive Roboticist <naiveroboticist@gmail.com>
2014-07-04 15:44:41 -0400

    Initial stab at signature manager.

---
+++ apps/telep/src/signature_handler.erl
@@ -0,0 +1,72 @@
+-module(signature_handler).
+-behavior(gen_server).
+
+-record(keys, 
+  { key_path,
+    private_key,
+    public_keys=[] }).
+
+-export([ start_link/2 ]).
+-export([ init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3 ]).
+
+start_link(KeyPath, PrivateKey) ->
+  { ok, Pid } = gen_server:start_link({global, signature}, ?MODULE, [ KeyPath, PrivateKey ], []),
+  % register(signature, Pid),
+  { ok, Pid }.
+
+init([ KeyPath, PrivateKey ]) ->
+  { ok, #keys{key_path=KeyPath, private_key={PrivateKey, null} } }.
+
+handle_call({ verify, SignedMessage }, 
+  _From,
+  S=#keys{key_path=KeyPath, public_keys=PublicKeys}) ->
+  [ Cmd, RobotName, Signature ] = string:tokens(SignedMessage, "|"),
+  DigSig = base64:decode(Signature),
+  Message = binary:list_to_bin(io_lib:format("~s|~s", [ Cmd, RobotName ])),
+  case lists:keyfind(RobotName, 1, PublicKeys) of
+    { RobotName, PublicKey } ->
+      { reply, verify_signature(Message, DigSig, PublicKey), S };
+    _ ->
+      % Unable to find a key registered to the robot,
+      % we need to attempt to load the key for it.
+      case load_key_for_robot(RobotName, KeyPath) of
+        { ok, Key } ->
+          NewState = S#keys{public_keys=[{RobotName, Key}|PublicKeys]},
+          { reply, verify_signature(Message, DigSig, Key), NewState };
+        _ ->
+          { reply, invalid, S }
+      end
+  end.
+
+handle_cast(_, State) ->
+  { noreply, State }.
+
+handle_info(_, State) ->
+  { noreply, State }.
+
+terminate(_Reason, _State) ->
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  State.
+
+% Private Methods
+
+verify_signature(Message, DigSig, Key) ->
+  case public_key:verify(Message, sha256, DigSig, Key) of
+    true -> Message;
+    false -> invalid
+  end.
+
+load_key_for_robot(RobotName, KeyPath) ->
+  FullPath = io_lib:format("~s/~s_public.pem", [ KeyPath, RobotName ]),
+  case filelib:is_regular(FullPath) of
+    true ->
+      { ok, PublicPemBin } = file:read_file(FullPath),
+      [ KeyEntry ] = public_key:pem_decode(PublicPemBin),
+      Key = public_key:pem_entry_decode(KeyEntry),
+      { ok, Key };
+    _ ->
+      false
+  end.
+
--- js/robot_client.js
+++ js/robot_client.js
@@ -1,23 +1,70 @@
 var net = require('net');
 var sprintf = require('sprintf').sprintf;
+var fs = require('fs');
+var crypto = require('crypto');
 
 var args = process.argv.slice(2);
 
 var client = new net.Socket();
 var first = true;
-// var name = "robot|Sammy";
-var name = args[0] || "Sammy";
+var name = args[0] || "minion";
 var registrationStr = sprintf("robot|%s", name);
+
+// Pull in the private key for the specific robot
+var privateKeyFile = "./" + name + "_private.pem";
+var privateKeyData = null;
+fs.readFile(privateKeyFile, 'ascii', function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    privateKeyData = data;
+});
+
+// Pull in the public key for the telep server
+var publicKeyFile = "./telep_public.pem";
+var publicKeyData = null;
+fs.readFile(publicKeyFile, 'ascii', function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    publicKeyData = data;
+});
+
+function verifyServerMessage(data) {
+  var cmps = data.split('|');
+  var message = cmps[0];
+  var signature = cmps[1];
+  var verifier = crypto.createVerify('RSA-SHA256');
+  verifier.update(message);
+  if (verifier.verify(publicKeyData, signature, 'base64')) {
+    return message;
+  } else {
+    return null;
+  }
+}
+
+function signMessage(message) {
+  var signer = crypto.createSign('RSA-SHA256');
+  signer.update(message);
+  var signature = signer.sign(privateKeyData, 'base64');
+  return signature + "|" + message;
+}
 
 client.connect(5555, 'daneel', function() {
     console.log('Connected');
 });
 
 client.on('data', function(data) {
-    console.log('Data: ' + data.slice(1));
-    if (first) {
-      client.write(sprintf("%c%s", registrationStr.length, registrationStr));
-      first = false;
+    var message = verifyServerMessage(data);
+    if (message) {
+      console.log('Data: ' + message);
+      if (first) {
+        client.write(signMessage(registrationStr));
+        // client.write(sprintf("%c%s", registrationStr.length, registrationStr));
+        first = false;
+      }
+    } else {
+      console.log("Invalid message from server: " + data);
     }
     // client.destroy();
 });
