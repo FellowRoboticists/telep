54276897d30c1ce943b309bb53384b8f18f29f3e
Naive Roboticist <naiveroboticist@gmail.com>
2014-07-23 19:32:34 -0400

    Added eunit tests for parse_name_value.

--- apps/telep/src/q_params.erl
+++ apps/telep/src/q_params.erl
@@ -6,9 +6,13 @@
 -module(q_params).
 -export([ parse_query_parameters/1 ]).
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 parse_name_value(Nvp) ->
   [ Name, Value ] = string:tokens(Nvp, "="),
-  { Name, Value }.
+  { string:strip(Name), string:strip(Value) }.
 
 convert_to_params([], NewList) ->
   NewList;
@@ -18,3 +22,13 @@ convert_to_params([Head|Tail], NewList) ->
 parse_query_parameters(QString) ->
   Nvps = string:tokens(QString, "&"),
   convert_to_params(Nvps, []).
+
+-ifdef(TEST).
+parse_name_value_test_() ->
+  [?_assertMatch({ "key", "value" }, parse_name_value("key=value")),
+   ?_assertMatch({ "key", "value" }, parse_name_value(" key = value ")),
+   ?_assertError({badmatch,["key|value"]}, parse_name_value("key|value")),
+   ?_assertError({badmatch,["key","value","other"]}, parse_name_value("key=value=other"))
+  ].
+-endif.
+
