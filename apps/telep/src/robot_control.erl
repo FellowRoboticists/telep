bbaf0a8c0905518b0b4f1a26231d25409f003f4d
Naive Roboticist <naiveroboticist@gmail.com>
2014-08-03 17:16:00 -0400

    Keypress robot control and version.
    Added the ability to control the robot using keypresses on the
    robot control page. Has some nice short cuts like hitting forward
    multiple times to speed up.
    
    Also now displaying the application version on the robot control
    page.

--- apps/telep/src/robot_control.erl
+++ apps/telep/src/robot_control.erl
@@ -3,8 +3,12 @@
 % robot_control.mustache template.
 %
 -module(robot_control).
--export([ registered_robots/ 0 ]).
+-export([ registered_robots/0, app_version/0 ]).
 
 registered_robots() ->
   Robots = gen_server:call(whereis(robot_db), { registered_robots }),
   [ dict:from_list([{ robot, RobotName }]) || RobotName <- Robots ].
+
+app_version() ->
+  {ok, Version } = application:get_key(telep, vsn),
+  Version.
--- www/assets/robot_control.js
+++ www/assets/robot_control.js
@@ -60,6 +60,61 @@ function issueCommand(command) {
   }
 }
 
+function handleKeypress(event) {
+  event.preventDefault();
+  switch (event.which) {
+    case 32: // Space
+      case 83: // S
+      case 115: // s
+        command = 'stop';
+        $("#currentCommand").val(command);
+        break;
+      case 70: // F
+      case 102: // f
+        if ($("#currentCommand").val() === 'forward') {
+          command = 'speed_up';
+        } else {
+          command = 'forward';
+          $("#currentCommand").val(command);
+        }
+        break;
+      case 66: // B
+      case 98: // b
+        if ($("#currentCommand").val() === 'backward') {
+          command = 'speed_up';
+        } else {
+          command = 'backward';
+          $("#currentCommand").val(command);
+        }
+        break;
+      case 82: // R
+      case 114: // r
+        if ($("#currentCommand").val() === 'rotate_cw') {
+          command = 'speed_up';
+        } else {
+          command = 'rotate_cw';
+          $("#currentCommand").val(command);
+        }
+        break;
+      case 76: // L
+      case 108: // l
+        if ($("#currentCommand").val() === 'rotate_ccw') {
+          command = 'speed_up';
+        } else {
+          command = 'rotate_ccw';
+          $("#currentCommand").val(command);
+        }
+        break;
+      case 68: // D
+      case 100: // d
+        command = 'slow_down';
+        $("#currentCommand").val(command);
+        break;
+    }
+    $("#theCommand").empty().append("<p><strong>Current Command:</strong> " + $("#currentCommand").val() + "<br><strong>Sent command:</strong> " + command);
+    issueCommand(command);
+}
+
 var ws = $.websocket(wsUri, {
   open: function(e) {
     $("#output").append("CONNECTED");
@@ -91,6 +146,8 @@ var ws = $.websocket(wsUri, {
  * Register the click-events to send the commands.
  */
 $( document ).ready(function() {
+
+  $(document).keypress(handleKeypress);
 
   // Handle the slow-down button
   $("#slow_down" ).click(function(event) { issueCommand("slow_down") });
