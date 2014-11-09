f1fb1d533bbc79dc2844dbcb022389267bcb2186
Naive Roboticist <naiveroboticist@gmail.com>
2014-11-09 12:49:24 -0500

    Added test.

---
+++ test/reverse_tests.erl
@@ -0,0 +1,6 @@
+-module(reverse_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+reverse_nil_test() -> [] = lists:reverse([]).
+reverse_one_test() -> [1] = lists:reverse([1]).
+reverse_two_test() -> [2,1] = lists:reverse([1,2]).
